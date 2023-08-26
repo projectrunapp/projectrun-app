a1d372d9efba6d8d0f0c5c0ae4904e7b3421f2b7
boolfalse <extravaganza1024@gmail.com>
2023-08-26 16:24:42 -0700

    Mapbox integration initials

--- app/components/RunMap.tsx
+++ app/components/RunMap.tsx
@@ -1,14 +1,17 @@
 
-import {Image} from "react-native";
-import {defaultMapImageUrl} from "../utils/constants";
+import React from 'react';
+import { View } from 'react-native';
+import Mapbox from '@rnmapbox/maps';
+import {MAPBOX_PUBLIC_TOKEN} from "@env"
+
+Mapbox.setAccessToken(MAPBOX_PUBLIC_TOKEN);
 
 const RunMap = ({ runId }: any) => {
     return (
-        <Image source={{uri: defaultMapImageUrl}} style={{
-            width: "100%",
-            height: 300,
-        }} alt="Run Map" />
+        <View style={{height: 300, width: 300}}>
+            <Mapbox.MapView style={{flex: 1}} />
+        </View>
     );
-};
+}
 
 export default RunMap;
